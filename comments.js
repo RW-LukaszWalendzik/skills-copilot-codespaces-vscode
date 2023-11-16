14bef1f0eca9f0951322b7b61486a9c82ec93d66
RW-LukaszWalendzik <135225671+RW-LukaszWalendzik@users.noreply.github.com>
2023-11-16 15:39:46 +0000

    test

--- comments.js
+++ comments.js
@@ -1,83 +1,25 @@
-//create web serverusing express
+//create web server and listen to port 3000
 const express = require('express');
 const app = express();
 const port = 3000;
 
-//use body parser to parse json
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-//use cors to allow cross origin resource sharing
-const cors = require('cors');
-app.use(cors());
-
-//use mysql to connect to database
-const mysql = require('mysql');
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'comment'
-});
-
 //connect to database
-db.connect((err) => {
-    if (err) {
-        throw err;
+const db = require('./config/mongoose');
+
+//use express router
+app.use(express.urlencoded());
+
+//use express router
+app.use('/',require('./routes'));
+
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+//listen to port
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
     }
-    console.log('Connected to database');
+    console.log(`Server is running on port: ${port}`);
 });
-
-//get all comments
-app.get('/api/comments', (req, res) => {
-    let sql = 'SELECT * FROM comment';
-    let query = db.query(sql, (err, results) => {
-        if (err) {
-            throw err;
-        }
-        res.send(JSON.stringify({
-            "status": 200,
-            "error": null,
-            "response": results
-        }));
-    });
-});
-
-//get single comment
-app.get('/api/comments/:id', (req, res) => {
-    let sql = 'SELECT * FROM comment WHERE id=' + req.params.id;
-    let query = db.query(sql, (err, result) => {
-        if (err) {
-            throw err;
-        }
-        res.send(JSON.stringify({
-            "status": 200,
-            "error": null,
-            "response": result
-        }));
-    });
-});
-
-//add new comment
-app.post('/api/comments', (req, res) => {
-    let data = {
-        name: req.body.name,
-        comment: req.body.comment,
-        date: req.body.date
-    };
-    let sql = 'INSERT INTO comment SET ?';
-    let query = db.query(sql, data, (err, result) => {
-        if (err) {
-            throw err;
-        }
-        res.send(JSON.stringify({
-            "status": 200,
-            "error": null,
-            "response": result
-        }));
-    });
-});
-
-//update comment
-app.put('/api/comments/:id', (req, res) => {
-    let sql = 'UPDATE comment SET name="' + req.body.name + '", comment="' + req.body.comment + '", date="' + req.body.date + '" WHERE id=' + req.params.id;
